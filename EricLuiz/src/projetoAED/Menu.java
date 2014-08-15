dd3c4533058cc7e7d063c8765284ac422e8aa322
eric <eric.lrf@gmail.com>
2014-08-15 06:00:58 -0300

    Atualização da Classe "Menu"

--- EricLuiz/src/projetoAED/Menu.java
+++ EricLuiz/src/projetoAED/Menu.java
@@ -8,8 +8,8 @@ public class Menu {
 	
 	ArrayList<Vertice> listaVertices = new ArrayList<Vertice>();
 	ArrayList<Aresta> listaArestas = new ArrayList<Aresta>();
-	Vertice vertice = new Vertice();
-	Aresta aresta = new Aresta();
+//	Vertice vertice = new Vertice();
+//	Aresta aresta = new Aresta();
 	
 	public Menu() {
 		super();
@@ -20,35 +20,34 @@ public class Menu {
 		String texto = "";
 		Menu menu = new Menu();
 		System.out.println("-Inserir Vértices-");
-		System.out.println("");
-		while (texto != "fim") {
+//		System.out.println("");
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Digite o nome do novo vertice: ");
 			texto = ler.next();
 			menu.inserirVertice(texto);
-			System.out.println("(Para encerrar, digite 'fim')");
-			System.out.println("");
+//			System.out.println("(Para encerrar, digite 'fim')");
+//			System.out.println("");
 		}
-		texto = "";
+//		texto = "";
 		System.out.println("-Inserir Arestas-");
-		System.out.println("");
-		while (texto != "fim") {
+//		System.out.println("");
+		for (int i = 0; i < 8; i++) {
+			int custo = 0, indice1 = 0, indice2 = 0;
 			menu.exibirVertices();
-			System.out.println("Escolha um dos vertices: ");
-			texto = ler.next();
-			menu.inserirAresta(texto);
-			System.out.println("Escolha outro vertice: ");
-			texto = ler.next();
-			menu.inserirAresta(texto);
+			System.out.println("Digite o número de um vertice: ");
+			indice1 = ler.nextInt();
+			System.out.println("Digite o número de outro vertice: ");
+			indice2 = ler.nextInt();
 			System.out.println("Digite o custo da aresta: ");
-			texto = ler.next();
-			menu.inserirAresta(Integer.parseInt(texto));
-			System.out.println("(Para encerrar, digite 'fim')");
-			System.out.println("");
+			custo = ler.nextInt();
+			menu.inserirAresta(custo, indice1, indice2);
+//			System.out.println("(Para encerrar, digite 'fim')");
+//			System.out.println("");
 		}
-		texto = "";
+//		texto = "";
 		System.out.println("-Exibir Grafo e sub-grafo-");
-		System.out.println("");
-		while (texto != "fim") {
+//		System.out.println("");
+		for (int i = 0; i < 3; i++)  {
 			System.out.println("[1] Exibir Grafo");
 			System.out.println("[2] Exibir Árvore de Extensão Mínima");
 			texto = ler.next();
@@ -65,44 +64,63 @@ public class Menu {
 				System.out.println("");
 				break;
 			}
-			System.out.println("(Para encerrar, digite 'fim')");
-			System.out.println("");
+//			System.out.println("(Para encerrar, digite 'fim')");
+//			System.out.println("");
 		}
 	}
 
 	private void exibirAEM() {
 		// TODO Auto-generated method stub
+		Menu menu = new Menu();
+		ArvoreGeradora arvoreGeradora = new ArvoreGeradora();
+		arvoreGeradora.setListaVertices(listaVertices);
+		arvoreGeradora.setListaArestas(listaArestas);
+		Grafo grafo = new Grafo(arvoreGeradora.getListaVerticesIncluidos(), arvoreGeradora.getListaArestasIncluidas());
+		System.out.println(grafo.toString());
 
 	}
 
 	private void exibirGrafo() {
 		// TODO Auto-generated method stub
+		Grafo grafo = new Grafo(listaVertices, listaArestas);
+		System.out.println(grafo.toString());
 
 	}
 
-	private void inserirAresta(int custo) {
+	private Vertice metodoUm(int indice) {
+		return listaVertices.get(indice);
+	}
+	
+	private Vertice metodoDois(int indice) {
+		return listaVertices.get(indice);
+	}
+	
+	private void inserirAresta(int custo, int indice1, int indice2) {
 		// TODO Auto-generated method stub
-
+		Aresta aresta = new Aresta();
+		aresta.setCusto(custo);
+		aresta.setVertice1(metodoUm(indice1));
+		aresta.setVertice1(metodoDois(indice2));
+		listaArestas.add(aresta);
 	}
-
-	private void inserirAresta(String texto) {
-		// TODO Auto-generated method stub
-
-	}
+		
 
 	private void exibirVertices() {
 		// TODO Auto-generated method stub
+		int i = 0;
 		Iterator<Vertice> iterator = listaVertices.iterator();
 		while (iterator.hasNext()) {
-			System.out.print(iterator.next());
-			System.out.print(" ");
-			
+			System.out.print(i + "-" + iterator.next());
+			System.out.print(", ");
+			i++;
 		}
+		System.out.println("");
 
 	}
 
 	private void inserirVertice(String texto) {
 		// TODO Auto-generated method stub
+		Vertice vertice = new Vertice();
 		vertice.setNome(texto);
 		listaVertices.add(vertice);
 
